10243ad8cdebc6c3f01c0bbc9a6bae056ebd133b
Huy Nguyen <nvhuybn@gmail.com>
2020-12-01 23:56:49 +0700

    update

---
+++ Record.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MarcoRecord
+{
+    partial class Record
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartRecord = new System.Windows.Forms.Button();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ckShutdown = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStopRecord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartRecord
+            // 
+            this.btnStartRecord.Location = new System.Drawing.Point(13, 13);
+            this.btnStartRecord.Name = "btnStartRecord";
+            this.btnStartRecord.Size = new System.Drawing.Size(130, 30);
+            this.btnStartRecord.TabIndex = 0;
+            this.btnStartRecord.Text = "Start Record";
+            this.btnStartRecord.UseVisualStyleBackColor = true;
+            this.btnStartRecord.Click += new System.EventHandler(this.btnStartRecord_Click);
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(108, 58);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(130, 20);
+            this.txtTime.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Time record (s)";
+            // 
+            // ckShutdown
+            // 
+            this.ckShutdown.AutoSize = true;
+            this.ckShutdown.Location = new System.Drawing.Point(108, 98);
+            this.ckShutdown.Name = "ckShutdown";
+            this.ckShutdown.Size = new System.Drawing.Size(15, 14);
+            this.ckShutdown.TabIndex = 3;
+            this.ckShutdown.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Auto Shutdown";
+            // 
+            // btnStopRecord
+            // 
+            this.btnStopRecord.BackColor = System.Drawing.SystemColors.Control;
+            this.btnStopRecord.Location = new System.Drawing.Point(150, 13);
+            this.btnStopRecord.Name = "btnStopRecord";
+            this.btnStopRecord.Size = new System.Drawing.Size(150, 30);
+            this.btnStopRecord.TabIndex = 5;
+            this.btnStopRecord.Text = "Stop Record";
+            this.btnStopRecord.UseVisualStyleBackColor = false;
+            this.btnStopRecord.Click += new System.EventHandler(this.btnStopRecord_Click);
+            // 
+            // Record
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 133);
+            this.Controls.Add(this.btnStopRecord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ckShutdown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.btnStartRecord);
+            this.Name = "Record";
+            this.Text = "Record";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartRecord;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox ckShutdown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStopRecord;
+    }
+}
+
---
+++ Record.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarcoRecord
+{
+    public partial class Record : Form
+    {
+        public Record()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        private void btnStartRecord_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(3000);
+                var time = txtTime.Text.To<int>();
+                //SendKeys.Send("{F9}");
+                // Process[] pname = Process.GetProcesses();
+                Process obs = Process.GetProcessesByName("obs64").FirstOrDefault();
+                Process chrome = Process.GetProcessesByName("chrome").FirstOrDefault();
+
+                if (obs != null && chrome != null)
+                {
+                    IntPtr obsh = obs.MainWindowHandle;
+                    IntPtr chromeh = chrome.MainWindowHandle;
+                    SetForegroundWindow(obsh);
+                    SendKeys.Send("{F9}");
+                    SetForegroundWindow(chromeh);
+                    if (time > 0)
+                    {
+                        Thread.Sleep(time * 1000);
+                        SetForegroundWindow(obsh);
+                        SendKeys.Send("{F10}");
+                    }
+                    if (ckShutdown.Checked)
+                    {
+                        MessageBox.Show("Tắt máy", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        //Process.Start("shutdown", "/s /t 0");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không tắt máy", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                //if (time > 0)
+                //{
+                //    Thread.Sleep(time * 1000);
+                //    //SetForegroundWindow(h);
+                //    SendKeys.Send("{F10}");
+                //}
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnStopRecord_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process p = Process.GetProcessesByName("obs64").FirstOrDefault();
+                if (p != null)
+                {
+                    IntPtr h = p.MainWindowHandle;
+                    SetForegroundWindow(h);
+                    SendKeys.Send("{F10}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
